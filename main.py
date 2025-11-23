bf731b7f76048b9eeae33a18f7b8a73b2524796e
lloydchang <lloydchang@gmail.com>
2025-11-23 01:15:37 -0800

    fix: error

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, HTTPException, BackgroundTask
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import FileResponse
+from starlette.background import BackgroundTask
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List, Dict, Any
